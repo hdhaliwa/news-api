cb2c23c4283bae6bc8ad5886860b388361cb5349
Hardeep Dhaliwal <hdhaliwal@thinkful.com>
2019-02-27 11:26:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const apiKey = '5bbce76230ca4d22a212c69f52a2b276';
+
+const searchURL = 'https://newsapi.org/v2/everything';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
+}
+
+function getNews(query, maxResults=10) {
+  const params = {
+    q: query,
+    language: "en",
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+  
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNews(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
